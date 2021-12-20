3ff4833a2e2498dad4f24d906c1c4c0f4aabd013
JiashengJing <jiasheng.jing@datareachable.com>
2021-12-20 17:43:04 +0800

    Init my file management app

---
+++ file-index.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import fs from "fs";
+import path from 'path';
+
+var app = express()
+
+// base file root dir
+const file_root = "./root";
+
+// get file list
+app.get('/files', (_request, response) => {
+    let files: string[] = [];
+    fs.readdirSync(file_root)
+    .forEach(file => { 
+        files.push(file)
+        console.log(file); 
+    })
+
+    response.send({
+        file_list: files
+    })
+})
+
+
+// get file list
+app.get('/download/:fileName', (request, response) => {
+    const { fileName } = request.params;
+    const filePath = path.join(file_root, fileName);    
+    if(fs.statSync(filePath).isFile){
+        response.download(filePath);
+    } 
+    else {
+        response.end("Sorry, file is not exist!");
+    }
+})
+
+//upload single file to server
+app.post('/files', function (request, response) {
+    response.send('upload file to server')
+})
+
+// start server and bind port
+app.listen(4040)
+console.log('启动4040')
